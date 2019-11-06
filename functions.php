1914fa08c15d914643b55ecd7d9132e5118010b5
brilliantpamela <brilliantpamela@yahoo.com>
2019-11-06 15:07:16 -0500

    Using Bootstrap with no element from underscores

--- functions.php
+++ functions.php
@@ -5,27 +5,46 @@
  * @package agency-website
  */
 
-if ( ! function_exist( 'agency_website_setup' ) ) :
-    function agency_website_setup(){
-      add_theme_support( 'automatic-feed-links' );
-      add_theme_support( 'title-tag' );
-      add_theme_support( 'post-thumbnails' );
-      add_theme_support( 'html5', array(
-        'search-form',
-        'comment-form',
-        'comment-list',
-        'gallery',
-        'caption',
-      ) );
-      add_theme_support( 'custom-logo', array(
-        'height'      => 250,
-        'width'       => 250,
-        'flex-width'  => true,
-        'flex-height' => true,
-      ) );
-      register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'agency-website' ),
-		) );
-    }
-endif;
-add_action( 'after_setup_theme', 'agency_website_setup' );
+function load_stylesheets()
+{
+
+      wp_register_style ('icon', get_template_directory_uri() . '/img/favicon.png', array(), 1, 'all');
+      wp_enqueue_style ('icon');
+
+      wp_register_style ('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), 1, 'all');
+      wp_enqueue_style ('bootstrap');
+
+      wp_register_style ('fontawesome', get_template_directory_uri() . 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', array(), 1, 'all');
+      wp_enqueue_style ('fontawesome');
+
+      wp_register_style ('Meanmenu', get_template_directory_uri() . '/css/meanmenu.css', array(), 1, 'all');
+      wp_enqueue_style ('Meanmenu');
+
+      wp_register_style ('Animation', get_template_directory_uri() . '/css/aos.min.css', array(), 1, 'all');
+      wp_enqueue_style ('Animation');
+
+      wp_register_style ('Owlcarousel', get_template_directory_uri() . '/css/owl.carousel.min.css', array(), 1, 'all');
+      wp_enqueue_style ('Owlcarousel');
+
+      wp_register_style ('Main', get_template_directory_uri() . '/style.css', array(), 1, 'all');
+      wp_enqueue_style ('Main');
+
+
+
+}
+
+/*
+
+    <!-- Google Font -->
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Raleway:400,500,700,900');
+    </style>
+   
+   
+  
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+  */
+
+  add_action('wp_enqueue_scripts', 'load_stylesheets' );
